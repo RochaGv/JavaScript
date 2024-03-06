4b861994182e029df4f1cb95fd69a4594463719c
RochaGv <gabrielrochatec@hotmail.com>
2024-03-06 17:20:06 -0300

    Implementação da função sumArrayElements

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,3 +4,32 @@ describe("index", () => {
     expect(soma(2, 2)).toEqual(4);
   });
 });
+
+
+import { sumArrayElements } from ".";
+describe("index", () => {
+    it('Calcule a soma de todos os elementos em um array.', () => {
+        //given
+        const entrada = [2, 3, 4, 5, 6];
+
+        //when
+        const result = sumArrayElements(entrada);
+
+        //then
+        expect(result).toBe(20);
+    });
+});
+
+import { sumArrayElements2 } from ".";
+describe("index", () => {
+    it('Calcule a soma de todos os elementos em um array.', () => {
+        //given
+        const entrada = [9, 8, 7, 6];
+
+        //when
+        const result = sumArrayElements2(entrada);
+
+        //then
+        expect(result).toBe(30);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 export function soma(a, b) {
-    return a + b
-    }
+  return a + b;
+}
+
+export function sumArrayElements(entrada) {
+
+  const result = entrada.reduce((acc, curr) => acc + curr, 0);
+
+  return result;
+}
+
+export function sumArrayElements2(entrada) {
+
+    const result = entrada.reduce((acc, curr) => acc + curr, 0);
+  
+    return result;
+  }
+  
